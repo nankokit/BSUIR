f9bbfd1b8fbad9b164d4f72377173c0cf28c66af
nankokit <130226365+nankokit@users.noreply.github.com>
2024-07-15 17:15:48 +0300

    Add files via upload

---
+++ 3sem/refs/КурсоваяКипяточка/парсер табуляции/par/par/par.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	ifstream file("code.txt");
+	ofstream final_file("final_code.txt");
+	do
+	{
+		//string str;
+		char str[256];
+		//getline(file, str);
+		file.getline(str, 256);
+		int j = 0;
+		while (str[j * 4] == ' ') j++;
+		//for (int i = j * 2; i < str.length(); i++)
+		for (int i = j * 2; str[i]; i++)
+			final_file << str[i];
+		final_file << '\n';
+	} while (!file.eof());
+
+	return 0;
+}
